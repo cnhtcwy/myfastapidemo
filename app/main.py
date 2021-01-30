f2976a51aac753c2b526d311c88ef6af52e9460b
wangyang <teacherwyang@163.com>
2021-01-31 01:33:11 +0800

    官方推荐命令

--- app/main.py
+++ app/main.py
@@ -8,4 +8,5 @@ async def root():
     return {"message": "Hello World"}
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    # 官方推荐是用命令后启动 uvicorn main:app --host=127.0.0.1 --port=8010 --reload
+    uvicorn.run(app='main:app', host="127.0.0.1", port=8010, reload=True, debug=True)
